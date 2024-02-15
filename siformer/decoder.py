32ee5eb21e93ff2378d5d4ea1df3896b47177603
Administrator <muxin.pu@monash.edu>
2024-02-15 21:48:17 +0800

    Move decoder to a separate file

---
+++ siformer/decoder.py
@@ -0,0 +1,40 @@
+import torch
+import torch.nn.functional as F
+from torch import Tensor, nn
+
+from typing import Optional, Union, Callable
+
+isChecked = False
+
+
+class DecoderLayer(nn.TransformerDecoderLayer):
+    """
+    Edited TransformerDecoderLayer implementation omitting the redundant self-attention operation as opposed to the
+    standard implementation.
+    """
+
+    def __init__(self, d_model: int, nhead: int, dim_feedforward: int = 2048, dropout: float = 0.1,
+                 activation: Union[str, Callable[[Tensor], Tensor]] = F.relu):
+        super(DecoderLayer, self).__init__(d_model, nhead, dim_feedforward, dropout, activation)
+        # Change self.multihead_attn to use Pro-sparse attention
+
+    def forward(self, tgt: torch.Tensor, memory: torch.Tensor, tgt_mask: Optional[torch.Tensor] = None,
+                memory_mask: Optional[torch.Tensor] = None, tgt_key_padding_mask: Optional[torch.Tensor] = None,
+                memory_key_padding_mask: Optional[torch.Tensor] = None, tgt_is_causal: Optional[bool] = False,
+                memory_is_causal: Optional[bool] = False) -> torch.Tensor:
+        global isChecked
+        if not isChecked:
+            print('Using custom DecoderLayer')
+            isChecked = True
+
+        tgt = tgt + self.dropout1(tgt)
+        tgt = self.norm1(tgt)
+        tgt2 = self.multihead_attn(tgt, memory, memory, attn_mask=memory_mask,
+                                   key_padding_mask=memory_key_padding_mask)[0]
+        tgt = tgt + self.dropout2(tgt2)
+        tgt = self.norm2(tgt)
+        tgt2 = self.linear2(self.dropout(self.activation(self.linear1(tgt))))
+        tgt = tgt + self.dropout3(tgt2)
+        tgt = self.norm3(tgt)
+
+        return tgt
